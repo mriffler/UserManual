1f0f0dfc74bb22de1781829a6e1bb258e5dcdc28
marpet <marco.peters@brockmann-consult.de>
2018-07-26 11:45:54 +0200

    removed githubpages config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ release = '0.1-pre'
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
