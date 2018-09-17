9885417f5318cb3e23aa577877909e08e9cfc1d8
Jan_W <jan.weversr@brockmann-consult.de>
2018-09-17 14:55:21 +0200

    copyright changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'S2GM User Manual'
-copyright = '2018, Brockmann Consult GmbH, Sinergise LTD., GeoVille GmbH'
+copyright = '2018, Copernicus Programme'
+# copyright = '2018, Brockmann Consult GmbH, Sinergise LTD., GeoVille GmbH'
 author = 'Brockmann Consult GmbH, Sinergise LTD., GeoVille GmbH'
 
 # The short X.Y version
